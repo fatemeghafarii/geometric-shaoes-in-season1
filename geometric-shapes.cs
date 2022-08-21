9b0c73da77fef7ba59c8a8fefb3b11f7ec45b45e
Fateme Ghafari <fatemeghafarii20@gmail.com>
2022-08-21 17:30:12 +0430

    geometric shapes with sequence escape

---
+++ geometric-shapes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Collections.Generic;
+using System.Collections;
+namespace MyFirstProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            Console.Write("    *\n   *** \n  ***** \n ******* \n********* ");
+            Console.WriteLine();
+            Console.Write("\n*\n**\n* *\n*  *\n*   *\n******");
+            Console.WriteLine();
+            Console.Write("\n    *\n   * * \n  *   * \n *     * \n********* ");
+            Console.WriteLine();
+            Console.Write("\n    *\n   * * \n  *   * \n *     * \n*\t*\n *     * \n  *   * \n   * * \n    * ");
+        }         
+    }
+}
+
